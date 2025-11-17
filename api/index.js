8daf12438a76bf3a6fb8e0121f1c7f8d6be6983a
Daniel <d97shryu@gmail.com>
2025-11-17 02:11:24 -0800

    redeploy

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
